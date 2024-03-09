3516eb8e82ce6677d23a8dc7567edb6b65c1735b
B0BB0 <riccardogobbo00@gmail.com>
2024-03-09 14:16:40 +0100

    webapp

---
+++ webapp/pages/vag2.py
@@ -0,0 +1,9 @@
+import streamlit as st #all streamlit commands will be available through the "st" alias
+
+st.set_page_config(page_title="Streamlit Demo") #HTML title
+st.title("Streamlit Demo") #page title
+
+
+
+
+st.text("Pagina2")
