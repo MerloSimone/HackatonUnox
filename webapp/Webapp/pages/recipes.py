ee7a450ade6f5afd4bc68b6dbb1cacb3463e2f08
B0BB0 <riccardogobbo00@gmail.com>
2024-03-10 05:05:23 +0100

    Try-except added

--- webapp/Webapp/pages/recipes.py
+++ webapp/Webapp/pages/recipes.py
@@ -16,9 +16,10 @@ with right_column:
 
 if custom_recip_butt: # and esiste l'indice con i file caricati
     folder_path = '../../user_product_index'
-    if os.listdir(folder_path):
+    try:
+        os.listdir(folder_path)
         st.switch_page("pages/custom_recipes.py")
-    else:
+    except:
         st.error("You have not uploaded any file yet")
         with st.spinner("Redirecting"):
             sleep(2)
